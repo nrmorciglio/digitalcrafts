e77c5b5010c7e6ded7f57e61f042c771ff1e214c
nrmorciglio <nicholasmorciglio@gmail.com>
2021-08-04 14:54:31 -0500

    Created nmW1D3Assignment1.py

---
+++ week1/day3/nmW1D3Assignment1.py
@@ -0,0 +1,19 @@
+num1 = int(input('Please enter a number\n'))
+num2 = int(input('Please enter a second number\n'))
+opera = input('Please enter an operand + , - , * , /\n')
+
+
+# print(num1,num1,opera)
+
+if opera == '+':
+    print(num1 + num2)
+if opera == '-':
+    print(num1 - num2)
+if opera == '*':
+    print(num1 * num2)
+if opera == '/':
+    print(num1 / num2)
+
+
+
+
