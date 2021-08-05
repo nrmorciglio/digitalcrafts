7280074e00be29ae38c0b05941704ad417525d0d
nrmorciglio <nicholasmorciglio@gmail.com>
2021-08-04 22:04:00 -0500

    Changed wording of else statement

--- week1/day3/nmW1D3Assignment3.py
+++ week1/day3/nmW1D3Assignment3.py
@@ -1,4 +1,4 @@
-num1 = int(input('Please enter a number that gives you both Fizz & Buzz!\n'))
+num1 = int(input('Please enter a number that gives you both Fizz & Buzz\n'))
 if (num1 % 3 == 0 and num1 % 5 == 0):
     print('Fizz Buzz')
 
@@ -9,6 +9,6 @@ elif (num1 % 5) == 0:
     print('Buzz')
 
 else:
-    print('Sorry! Try again!')
+    print('Sorry! Run this program once more and try again')
 
 
